1cdbefb2b32105ef34d90b13ea53d6d013dd46aa
Charlie Cheever <ccheever@gmail.com>
2015-07-02 14:02:56 -0700

    Adding check repeatedly script

---
+++ check-repeatedly.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var timeconstants = require('timeconstants');
+
+setInterval(function () {
+  require('.')().then(function () {
+    console.log("Check for new iojs version");
+  }, function (err) {
+    console.error("*ERR* Failed to check for new iojs version:", err, "\n" + err.stack);
+  });
+}, timeconstants.minute * 2);
