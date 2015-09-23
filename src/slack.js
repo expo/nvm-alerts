ae272972065a24145d93982f1b9784d4280594d6
Charlie Cheever <ccheever@gmail.com>
2015-09-22 19:23:17 -0700

    Changing slack message to something that will actually install the latest versions of node

--- src/slack.js
+++ src/slack.js
@@ -18,9 +18,10 @@ function sendSlackWebhookMessageAsync(options) {
 }
 
 async function sendNewNodeVersionMessageAsync(version) {
+  let v = 'v' + version.format();
   return await sendSlackWebhookMessageAsync({
     text: `There's a new version of Node.js available: ${version.format()}\n` +
-      `Get it with:\n\`nvm install iojs\`\n` +
+      `Get it with:\n\`nvm install ${v}; nvm alias default ${v}\`\n` +
       `(nvm is available at: https://github.com/creationix/nvm )`,
   });
 }
